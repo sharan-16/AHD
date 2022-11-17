d13b16a2d2b95ef1b73a17395af6c9951e240a3d
sharan-16 <110844824+sharan-16@users.noreply.github.com>
2022-11-16 21:54:17 -0500

    Update REG.v

--- REG.v
+++ REG.v
@@ -1,34 +1,36 @@
 // Code your design here
+// Code your design here
 module REGISTERS( 
-  input wire readaddr1,
-  input wire readaddr2,
-  input wire writeaddr,
-  input wire write_data,
+  input wire [4:0] readaddr1,
+  input wire [4:0] readaddr2,
+  input wire [4:0] writeaddr,
+  input wire [31:0] write_data,
   input wire write_cntrl,
   input clk,
   input clr,
-  output reg read1,
-  output reg read2
+  output reg [31:0] read1,
+  output reg [31:0] read2
 );
   
-  reg [31:0][31:0]REG;
-  integer i=0;
+  reg [31:0]REG[31:0];
+  genvar i;
   
-  assign REG[0]=0;
-  
+  assign REG[0]=32'd0;
   assign read1=REG[readaddr1];
   assign read2=REG[readaddr2];    
-  
-  always @(posedge clk or posedge clr) begin
-    if (clr==1) begin
-      for(i=0;i<32;i++) begin
-        REG[i]<=0;
-      end
-    end
-    else if (write_cntrl==1) begin
-      REG[writeaddr]=writedata;
-      
-    end
-  end
-  
+
+  generate 
+    for (i=1; i<31; i=i+1) begin
+      always @(posedge clk or posedge clr) begin
+        if (clr==1) begin
+          REG[i]<=32'd0;
+        end
+        else if (write_cntrl==1) begin
+            if (i == writeaddr) begin
+              REG[i] = write_data;
+            end
+        end
+      end  
+    end  
+  endgenerate      
 endmodule
